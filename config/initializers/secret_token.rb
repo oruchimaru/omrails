8564ca571d969964cd1df91b5648868d01aa49c3
Uchiha Sasuke <ripplepimail@gmail.com>
2013-03-05 17:58:25 -0500

    Intial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '584ffd0db8739286e9f61ad7d7c62df42d99f5ebf2b9c41ba642615cbaebd84937a2ff0566459cb9650a42c1b13cfa923cdf6d5ad87c32ccb697c2a26ae53309'
